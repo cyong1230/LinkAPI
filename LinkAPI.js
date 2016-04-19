405244922518a96fbd4602757e48217508c71317
cyong1230 <cyong1230@yahoo.com>
2016-04-19 15:45:06 +0800

    Update LinkAPI userscript

--- LinkAPI.js
+++ LinkAPI.js
@@ -4,14 +4,14 @@
 // @version      0.7
 // @description  API linking for S-NER project
 // @author       Chee Yong
-// @include      http://stackoverflow.com/*
-// @include      stackoverflow.com/*
-// @include      https://stackoverflow.com/*
+// @include      http://stackoverflow.com/questions/*
+// @include      stackoverflow.com/questions/*
+// @include      https://stackoverflow.com/questions/*
 // @require      https://raw.githubusercontent.com/padolsey/findAndReplaceDOMText/master/src/findAndReplaceDOMText.js
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
 // @require      https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
 // @resource     jqueryCSS https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css
-// @grant	     GM_getResourceText
+// @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @connect      127.0.0.1
@@ -39,13 +39,13 @@ function identifyAPI() {
     );
 
     // manual mode (custom list of recognized APIs)
-    /*
-    var entityJSON = ['eigvals'];
+    
+    var entityJSON = ['astype'];
     extractEntity(entityJSON);
-    */
+    
 
     // auto mode (access NER model)
-    
+    /*
     GM_xmlhttpRequest({
         method: "POST",
         // url: "http://127.0.0.1:8000/extractentity/", //localhost
@@ -60,7 +60,7 @@ function identifyAPI() {
                     var entityJSON = JSON.parse(response.responseText);
                     extractEntity(entityJSON);
                 } catch(e) {
-                    alert('Something went wrong with entity recognition :-(');
+                    console.log('Something went wrong with entity recognition :-(');
                     return;
                 }
             }
@@ -76,7 +76,7 @@ function identifyAPI() {
             console.log("Connection to server aborted! (entity recognition)");
         }
     });
-
+    */
 }
 
 /* Extract API in web page */
@@ -249,7 +249,7 @@ function UpdateTooltip(response) {
             linkJSON = JSON.parse(response.responseText);
             console.log(linkJSON);
         } catch(e) {
-            alert("Something went wrong with entity linking :-(");
+            console.log("Something went wrong with entity linking :-(");
             linkError = true;
             return;
         }
@@ -265,7 +265,7 @@ function UpdateTooltip(response) {
         } else if (linkJSON[i].length == 1) {
             contentStr = "<!DOCTYPE html><html><head><style type='text/css'>";
             contentStr += ".myTable {width:100%;word-wrap:break-word;word-break:break-all;border-spacing:0;border-collapse:collapse;font-size:12px} .myTable th, .myTable td {border: 1px solid black;text-align:center;} .table-scroll {max-width:820px;max-height:250px;overflow-y:scroll;overflow-x:hidden;}</style></head><body>";
-            contentStr += "<div class='table-scroll'><table class='myTable'><colgroup><col width='40'><col width='200'><col width='60'><col width='60'></colgroup><tr><th>Rank</th><th>Reference Link</th><th>Type</th><th>Library</th></tr>";
+            contentStr += "<div class='table-scroll'><table class='myTable'><colgroup><col width='40'><col width='180'><col width='60'><col width='60'></colgroup><tr><th>Rank</th><th>Reference Link</th><th>Type</th><th>Library</th></tr>";
             contentStr += "<tr><td>" + 1 + "</td><td><a href='" + linkJSON[i][0].url + "' target='_blank'>" + linkJSON[i][0].name + "</a></td><td>" + linkJSON[i][0].type + "</td><td>" + linkJSON[i][0].lib + "</td>";
             contentStr += "</table><div></body></html>";
             LinkMap[i] = contentStr;
@@ -276,7 +276,7 @@ function UpdateTooltip(response) {
             });
             contentStr = "<!DOCTYPE html><html><head><style type='text/css'>";
             contentStr += ".myTable {width:100%;word-wrap:break-word;word-break:break-all;border-spacing:0;border-collapse:collapse;font-size:12px;} .myTable th, .myTable td {border: 1px solid black;text-align:center;} .table-scroll {max-width:820px;max-height:250px;overflow-y:scroll;overflow-x:hidden;}</style></head><body>";
-            contentStr += "<div class='table-scroll'><table class='myTable'><colgroup><col width='35'><col width='200'><col width='50'><col width='60'><col width='30'><col width='50'><col width='40'><col width='30'><col width='40'><col width='40'></colgroup><tr><th>Rank</th><th>Reference Link</th><th>Type</th><th>Library</th><th>URL</th><th>Match</th><th>Tag</th><th>Title</th><th>Class</th><th>tf-idf</th></tr>";
+            contentStr += "<div class='table-scroll'><table class='myTable'><colgroup><col width='35'><col width='180'><col width='50'><col width='60'><col width='30'><col width='50'><col width='40'><col width='40'><col width='40'><col width='40'></colgroup><tr><th>Rank</th><th>Reference Link</th><th>Type</th><th>Library</th><th>URL</th><th>Match</th><th>Tag</th><th>Title</th><th>Class</th><th>tf-idf</th></tr>";
             for (var j = 0; j < linkJSON[i].length; j++) {
                 contentStr += "<tr><td>" + (j + 1) + "</td><td><a href='" + linkJSON[i][j].url + "' target='_blank'>" + ((linkJSON[i][j].api_class) ? linkJSON[i][j].api_class : "") + '.' + linkJSON[i][j].name + "</a></td><td>" + linkJSON[i][j].type + "</td><td>" + linkJSON[i][j].lib + "</td><td>" + ((linkJSON[i][j].mark[0]) ? "&#10004;" : "")  + "</td><td>" + ((linkJSON[i][j].mark[1]) ? "&#10004;" : "") + "</td><td>" + ((linkJSON[i][j].mark[2]) ? "&#10004;" : "") + "</td><td>" + ((linkJSON[i][j].mark[3]) ? "&#10004;" : "") + "</td><td>" + ((linkJSON[i][j].mark[4]) ? "&#10004;" : "") + "</td><td>" + Math.round(linkJSON[i][j].tfidf * 1000) / 1000 + "</td>";
             }
